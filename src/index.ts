cf4d25b418513e6e481e66b9856d59794e97384e
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-01 14:50:33 +0700

    Refactor fetch method to simplify R2Explorer usage

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import { R2Explorer } from "r2-explorer";
 
 export default {
-  fetch(req: Request, env: Env): Promise<Response> {
-    const url = new URL(req.url);
-
-    // ✅ biarkan request ini lewat tanpa auth
-    if (url.pathname === "/favicon.ico") {
-      return new Response(null, { status: 204 });
-    }
-
-    return R2Explorer({
+  fetch: (req: Request, env: Env): Promise<Response> =>
+    R2Explorer({
       readonly: false,
       basicAuth: {
         username: env.BASIC_AUTH_USER,
         password: env.BASIC_AUTH_PASS,
       },
-    }).fetch(req, env);
-  },
+    }).fetch(req, env),
 };
 
 interface Env {
