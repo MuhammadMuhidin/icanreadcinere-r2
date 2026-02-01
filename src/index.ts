414ea54b28972f1ee56f35f432287fd3024da3eb
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-01 11:51:35 +0700

    Add basic authentication to R2Explorer configuration

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
 	// Set to false to allow users to upload files
 	readonly: false,
-
+	basicAuth: {
+    	username: 'mita',
+    	password: '1235678'
+  	}
 	// Learn more how to secure your R2 Explorer instance:
 	// https://r2explorer.com/getting-started/security/
 	// cfAccessTeamName: "my-team-name",
